2087603f117743ee5fd7cd77435d29b40eeb375a
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2024-09-06 13:15:21 +0530

    Refactor form submission logic and add input validation

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -19,23 +19,28 @@ const UserForm = () => {
         const inputElement = e.target;
         inputElement.setCustomValidity(`Invalid! ${e.target.name}, Please enter a valid ${e.target.name}`)
     }
-    
-    
+
+    const handlePhoneNumber = (e) => {
+        if (/^\d*$/.test(e.target.value)) {
+            setPhoneNumber(e.target.value)
+        }
+    }
+
 
     return (
         <div className=''>
             <form onSubmit={handleFormData}>
                 <div className=''>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" id='name' name="name" required onChange={(e) => setName(e.target.value)} />
+                    <input type="text" id='name' name="name" required onChange={(e) => setName(e.target.value)} onInvalid={handleInvalid} value={name} />
                 </div>
                 <div className=''>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" id='email' name="email" required onChange={(e) => setEmail(e.target.value)} onInvalid={handleInvalid} />
+                    <input type="email" id='email' name="email" required onChange={(e) => setEmail(e.target.value)} onInvalid={handleInvalid} value={email} />
                 </div>
                 <div className=''>
                     <label htmlFor="phoneNumber">Phone No: </label>
-                    <input type="tel" id='phoneNumber' name="phoneNumber" onChange={(e) => setPhoneNumber(e.target.value)} required />
+                    <input type="text" id='phoneNumber' name="phoneNumber" onChange={handlePhoneNumber} required onInvalid={handleInvalid} maxLength="10" value={phoneNumber} />
                 </div>
                 <div >
                     <input className='px-5 py-2 rounded-full border border-solid	bg-violet-500 hover:bg-violet-400 active:bg-violet-600 font-semibold text-white shadow-md cursor-pointer' type="submit" value="Submit" />
